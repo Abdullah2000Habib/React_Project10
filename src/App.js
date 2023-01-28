992405cc818d5304752de8952154abba3733cc73
Abdullah2000Habib <abd3habib@gmail.com>
2023-01-28 12:57:56 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Home from './components/Home';
+import "./main.css";
+
+
+function App() {
+  return (
+    <div>
+      Abdullah Kamel Habib
+    </div>
+  );
+  }
+export default App;
